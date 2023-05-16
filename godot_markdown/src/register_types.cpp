df219257040a7dd36f05fa9f157ba4c08231623f
Fahrstuhl <fahrstuhl@users.noreply.github.com>
2023-05-16 23:33:14 +0200

    port to release gdextension API

--- godot_markdown/src/register_types.cpp
+++ godot_markdown/src/register_types.cpp
@@ -40,17 +40,23 @@
 
 using namespace godot;
 
-void register_cmark_types(void *userdata, GDExtensionInitializationLevel p_level) {
+void register_cmark_types(ModuleInitializationLevel p_level) {
+	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+		return;
+	}
 	ClassDB::register_class<Cmark>();
 }
 
-void unregister_cmark_types(void *userdata, GDExtensionInitializationLevel p_level) {}
+void unregister_cmark_types(ModuleInitializationLevel p_level) {}
 
 extern "C" {
 
-GDExtensionBool cmark_library_init(const GDExtensionInterface *p_interface, const GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
-	r_initialization->initialize = &register_cmark_types;
-	r_initialization->deinitialize = &unregister_cmark_types;
-	return 1;
+GDExtensionBool GDE_EXPORT cmark_library_init(const GDExtensionInterface *p_interface, const GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
+	godot::GDExtensionBinding::InitObject init_obj(p_interface, p_library, r_initialization);
+	init_obj.register_initializer(register_cmark_types);
+	init_obj.register_terminator(unregister_cmark_types);
+	init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
+
+	return init_obj.init();
 }
 }
